02cf7a68e80265de419781f32d70b692d60661ab
Panos <bairamispanagiotis@gmail.com>
2019-11-10 11:04:20 +0200

    components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Body = () => (
+  <p>
+    Hi! Edit <code>src/App.js</code> and save to reload.
+</p>
+);
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    <Body />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
