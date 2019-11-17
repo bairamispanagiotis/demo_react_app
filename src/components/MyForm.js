0395b9d04f0de6ead9089bbecbf6d6ea5ae25e78
Panos <bairamispanagiotis@gmail.com>
2019-11-17 14:08:22 +0200

    Form Validation

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+const initialState = {
+    name: '',
+    surname: '',
+    password: '',
+    email: '',
+    nameError: '',
+    surnameError: '',
+    passwordError: '',
+    emailError: ''
+}
+
+
 export default class MyForm extends React.Component {
 
-    state = {
-        name: '',
-        rememberMe: '',
-        title: 'Test'
-    }
+   
+    state = initialState;
 
     handleChange = (event) => {
         const isCheckbox = event.target.name === 'checkbox';
@@ -15,21 +24,66 @@ export default class MyForm extends React.Component {
 
     handleSubmit = event => {
         event.preventDefault();
-        console.log(this.state);
-    }
+       
+        const isValid = this.validate();
+       
+        if (isValid) {
+            console.log(this.state);
+            this.setState(initialState);
+            //clear form
+        }
+    };
+
+    validate = () => {
+      
+        let emailError = '';
+        let passwordError = '';
+        let nameError = '';
+        let surname = '';
+       
+        if (!this.state.email.includes('@')) {
+          
+            emailError = 'Invalid email';
+        }
+
+        if(!this.state.name){
+            nameError = "name cannot be blank";
+        }
+
+        if (emailError) {
+            this.setState({ emailError });
+            return false;
+        }
+
+        if (nameError) {
+            this.setState({ nameError });
+            return false;
+        }
+
+
+        return true;
+    };
 
     render() {
-        return <form onSubmit={this.handleSubmit}>
-            <input name="name" value={this.state.name} onChange={this.handleChange} />
-            <input name="rememberMe" onChange={this.handleChange} type="checkbox" checked={this.state.rememberMe} />
+        return <form noValidate onSubmit={this.handleSubmit}>
             <div>
-                <select name="title" value={this.state.title} onChange={this.handleChange}>
-                    <option>Test</option>
-                    <option>Test2</option>
-                    <option>Test3</option>
-                </select>
+                <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+                <div style={{ fontSize: 12, color: "red" }}>{this.state.nameError}</div>
             </div>
+            <div>
+                <input type="text" name="surname" value={this.state.surname} onChange={this.handleChange} />
+                <div style={{ fontSize: 12, color: "red" }}>{this.state.surnameError}</div>
+            </div>
+            <div>
+                <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
+                <div style={{ fontSize: 12, color: "red" }}>{this.state.emailError}</div>
+            </div>
+            <div>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                <div style={{ fontSize: 12, color: "red" }}>{this.state.passwordError}</div>
+            </div>
+
             <button type="submit">Submit</button>
         </form>
-    }
+    };
 }
